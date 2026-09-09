f86c82411a332f13fbad7ab3c887dbe047fa2779
g4mless <g4mless@proton.me>
2026-09-09 22:43:19 +0700

    hapus tombol lonceng notifikasi di topbar

--- resources/views/partials/topbar.blade.php
+++ resources/views/partials/topbar.blade.php
@@ -20,17 +20,6 @@
         @endisset
     </div>
 
-    {{-- Notification bell --}}
-    <button class="relative inline-flex items-center justify-center rounded-lg p-2 text-gray-500 hover:bg-gray-100">
-        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"/>
-        </svg>
-        <span class="absolute right-1.5 top-1.5 flex h-2 w-2">
-            <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75"></span>
-            <span class="relative inline-flex h-2 w-2 rounded-full bg-red-500"></span>
-        </span>
-    </button>
-
     {{-- User dropdown --}}
     <div class="relative">
         <button
